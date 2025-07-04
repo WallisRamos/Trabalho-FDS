1bb395b2f8e29abd6edf448c870f273a7e0f871e
WallisRamos <wallissgr@gmail.com>
2025-07-03 21:25:27 -0300

    Add files via upload

---
+++ JvScript/JvS.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector("#botao");
+let texto = document.querySelector("#mensagem");
+
+
+botao.addEventListener("click",e =>{
+
+texto.innerHTML="''A mente inquieta busca incessantemente o conhecimento, pois sabe que a jornada da sabedoria é infinita e mágica.''";
+
+});
